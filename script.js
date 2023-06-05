5803727978bcdf917385e86a8b17b90c3dcf4738
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-06-05 14:55:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// let container=document.createElement("div");
+// container.classList.add=("container");
+let container=document.createElement("div");
+container.classList.add=("container");
